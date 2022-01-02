b4efda7513866254cf0c6f02b2ced1e13a9262ed
Nedim Grmz <neodimjv@gmail.com>
2022-01-02 14:51:43 +0300

    dowhile

---
+++ DoWhileLoop.java
@@ -0,0 +1,22 @@
+package netgithub2;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=0;
+		while (n<=20) {
+			n+=2;
+			System.out.print(n+" / ");
+		}
+		System.out.println("\n");
+		int s=1;
+		do {
+			System.out.print(s + " * ");
+			s++;
+		} while (s<10);
+
+	}
+
+}
