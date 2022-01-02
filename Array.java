ee154f5144082db07adfdbbcc665445a03e0e3d0
Nedim Grmz <neodimjv@gmail.com>
2022-01-02 16:05:43 +0300

    arr

---
+++ Array.java
@@ -0,0 +1,27 @@
+package netgithub2;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String arr[]=new String[4];
+		arr[0]="Cem";
+		arr[1]="Ekrem";
+		arr[2]="Cemil";
+		arr[3]="Ekremiye";
+		
+		System.out.println(arr[0]);
+		
+		String arr2[]= {"Ali","Ahmet","Aslan"};
+		arr2[1]="Asiye";
+		
+		System.out.println(arr2[1]);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ forStringİnt.java
@@ -0,0 +1,18 @@
+package netgithub2;
+
+public class forStringÝnt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sayý=10;
+		String isim="Cem";
+		for (int i = 0; i < 8; i++) {
+			
+		System.out.println(sayý +" "+ isim);
+		}
+		
+
+	}
+
+}
